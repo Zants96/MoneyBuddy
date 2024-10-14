82908439f75d9c66904dccf0c31aa314e25da697
Leandro Lesnik <leandro.lesnik@gmail.com>
2024-10-13 23:42:59 -0300

    adicionado a interface dos dados

--- interface/types.ts
+++ interface/types.ts
@@ -1,15 +1,27 @@
-export interface ICategory {
+export interface ICategoria {
   id: number;
   nome: string;
-  tipo: "Despesa" | "Receita";
+  tipo: "Receita" | "Despesa";
 }
 
-export interface ITransaction {
-  id: number;
-  tipo: "Despesa" | "Receita";
+export interface ITransacao {
+  id?: number;
+  tipo: "Receita" | "Despesa" | string;
   total: number;
   categoria_id: number;
   data: string;
-  category?: string;
+  categoria: string;
   descricao: string;
+}
+
+export interface ITransacoesContextProps {
+  transacoes: ITransacao[];
+  categorias: ICategoria[];
+  addTransacao: (transaction: ITransacao) => void;
+  fetchTransacoes: () => void;
+  fetchCategorias: () => void;
+  deleteTransacao: (id: number) => void;
+  totalReceita: number;
+  totalDespesa: number;
+  saldo: number;
 }
